691409863227775dc27c821d7e8fc9071283f420
Костя Осадчий <qwrhevsjjdb83736@gmail.com>
2025-03-08 15:44:32 +0200

    Добавьте файлы проекта.

---
+++ Lab1.2/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿using System;
+
+namespace StudentSurvey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtBirthYear = new System.Windows.Forms.TextBox();
+            this.txtGroup = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.rtbResults = new System.Windows.Forms.RichTextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ім’я";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Прізвище";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Рік народження";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Група";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Курс";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(265, 31);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 5;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(265, 80);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 6;
+            // 
+            // txtBirthYear
+            // 
+            this.txtBirthYear.Location = new System.Drawing.Point(265, 129);
+            this.txtBirthYear.Name = "txtBirthYear";
+            this.txtBirthYear.Size = new System.Drawing.Size(100, 20);
+            this.txtBirthYear.TabIndex = 7;
+            // 
+            // txtGroup
+            // 
+            this.txtGroup.Location = new System.Drawing.Point(265, 188);
+            this.txtGroup.Name = "txtGroup";
+            this.txtGroup.Size = new System.Drawing.Size(100, 20);
+            this.txtGroup.TabIndex = 8;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(265, 229);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(100, 20);
+            this.txtCourse.TabIndex = 9;
+            // 
+            // rtbResults
+            // 
+            this.rtbResults.Location = new System.Drawing.Point(265, 281);
+            this.rtbResults.Name = "rtbResults";
+            this.rtbResults.ReadOnly = true;
+            this.rtbResults.Size = new System.Drawing.Size(100, 96);
+            this.rtbResults.TabIndex = 10;
+            this.rtbResults.Text = "";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(32, 281);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 11;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click_1);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(32, 354);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.rtbResults);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtGroup);
+            this.Controls.Add(this.txtBirthYear);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void btnOK_Click_1(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            // Ваш код для кнопки OK
+        }
+
+        private void btnClose_Click(EventArgs e,
+                                    object sender) => this.Close(); // Закриває форму
+        private void BtnOK_Click(
+            object sender, EventArgs e)
+        {
+            // Ваш код
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtBirthYear;
+        private System.Windows.Forms.TextBox txtGroup;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.RichTextBox rtbResults;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Lab1.2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StudentSurvey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Установлюємо заголовок форми
+            this.Text = "Лаб. 1";
+        }
+
+        private TextBox GetTxtGroup()
+        {
+            return txtGroup;
+        }
+
+        // Обробник натискання кнопки OK
+        private void btnOK_Click(object sender, EventArgs e, TextBox txtGroup)
+        {
+            // Читання даних з текстових полів
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+            string birthYear = txtBirthYear.Text;
+            string group = txtGroup.Text;
+            string course = txtCourse.Text;
+
+            // Перевірка на коректність даних
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName) ||
+                string.IsNullOrWhiteSpace(birthYear) || string.IsNullOrWhiteSpace(group) ||
+                string.IsNullOrWhiteSpace(course))
+            {
+                MessageBox.Show("Будь ласка, заповніть всі поля!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Перевірка на коректність року народження
+            if (!int.TryParse(birthYear, out int year))
+            {
+                MessageBox.Show("Невірний формат року народження!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Виведення результатів в багаторядкове поле
+            rtbResults.Text = $"Ім’я: {firstName}\nПрізвище: {lastName}\nРік народження: {birthYear}\nГрупа: {group}\nКурс: {course}";
+        }
+
+        // Обробник натискання кнопки Close
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            // Закриття форми
+            this.Close();
+        }
+
+        // Обробники подій для реагування на клавіатуру
+        private void btnOK_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnOK.PerformClick();
+            }
+        }
+
+        private void btnClose_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                btnClose.PerformClick();
+            }
+        }
+    }
+}
